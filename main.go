5c5e6e4a1041dfa59a4105ed9a8efbdbaa117711
ahmedtouahria <ahmedtouahria2001@gmail.com>
2024-03-14 23:30:19 +0100

    add ETH token identification

--- main.go
+++ main.go
@@ -10,25 +10,38 @@ import (
 )
 
 func main() {
-	// Replace with your Ethereum node URL
-	nodeURL := "https://eth.public-rpc.com"
-	client, err := ethclient.Dial(nodeURL)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Replace with your Ethereum node URL
+    nodeURL := "https://eth.public-rpc.com"
+    client, err := ethclient.Dial(nodeURL)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	// Replace with the actual Unipool contract address
-	contractAddress := common.HexToAddress("0xd3d2e2692501a5c9ca623199d38826e513033a17")
-	unipoolContract, err := unipool.NewUnipool(contractAddress, client)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Replace with the actual Unipool contract address
+    contractAddress := common.HexToAddress("0x2Bc3368e7876BE81C359dfAd7453ddDE1D13a4EB")
+    unipoolContract, err := unipool.NewUnipool(contractAddress, client)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	// Call GetReserves function
-	reserves, err := unipoolContract.GetReserves(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Call Token0 and Token1 functions to get token addresses
+    token0, err := unipoolContract.Token0(nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Printf("reserve 0 : %v, reserver 1 : %v \n", reserves.Reserve0, reserves.Reserve1)
+    token1, err := unipoolContract.Token1(nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    var ethTokenAddress = common.HexToAddress("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
+
+    if token0 == ethTokenAddress {
+        fmt.Println("Token 0 represents ETH")
+    } else if token1 == ethTokenAddress {
+        fmt.Println("Token 1 represents ETH")
+    } else {
+        fmt.Println("Neither token represents ETH")
+    }
 }
